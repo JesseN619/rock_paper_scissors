992cc19b284f0f9621ae0a7cbb9aaadd5fe5cccf
Jesse Nauman <jnauman619@gmail.com>
2020-12-13 12:35:45 -0600

    Add CSS styles

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ function playRound(playerSelection, computerSelection) {
         cpuScore++;
         document.getElementById("cpu-score").innerHTML = cpuScore;
     } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
-        document.getElementById("results").innerHTML = "Winner Winner Chicken Dinner! Rock beats Scissors";
+        document.getElementById("results").innerHTML = "Winner! Rock beats Scissors";
         let userScore = parseInt(document.getElementById("user-score").innerHTML);
         userScore++;
         document.getElementById("user-score").innerHTML = userScore;
     } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
-        document.getElementById("results").innerHTML = "Winner Winner Chicken Dinner! Paper beats Rock";
+        document.getElementById("results").innerHTML = "Winner! Paper beats Rock";
         let userScore = parseInt(document.getElementById("user-score").innerHTML);
         userScore++;
         document.getElementById("user-score").innerHTML = userScore;
@@ -41,7 +41,7 @@ function playRound(playerSelection, computerSelection) {
         cpuScore++;
         document.getElementById("cpu-score").innerHTML = cpuScore;
     } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
-        document.getElementById("results").innerHTML = "Winner Winner Chicken Dinner! Scissors beats Paper";
+        document.getElementById("results").innerHTML = "Winner! Scissors beats Paper";
         let userScore = parseInt(document.getElementById("user-score").innerHTML);
         userScore++;
         document.getElementById("user-score").innerHTML = userScore;
@@ -50,41 +50,55 @@ function playRound(playerSelection, computerSelection) {
     let roundNumber = parseInt(document.getElementById("round-number").innerHTML);
 
     if (roundNumber == 5) {
-        document.getElementById("results").innerHTML = "";
-        // Change results message from round results to game results
         let wins = parseInt(document.getElementById("user-score").innerHTML);
         let losses = parseInt(document.getElementById("cpu-score").innerHTML);
-        //TODO: Fade screen behind alert box
-        //TODO: change alert to confirm?
+        //TODO: Fade rest of screen
         if (wins > losses) {
-            alert("YOU WON THE GAME!\nClick 'OK' to play again.");
+            document.getElementById("results").innerHTML = "YOU WON THE GAME!";
         } else if (wins < losses) {
-            alert("YOU LOST THE GAME\nClick 'OK' to play again.");
+            document.getElementById("results").innerHTML = "YOU LOST THE GAME";
         } else if (wins == losses) {
-            alert("TIE GAME\nClick 'OK' to play again.");
+            document.getElementById("results").innerHTML = "TIE GAME";
         }
-        // Ask if they would like to play again
-        // Reset roundNumber to 1
-        document.getElementById("round-number").innerHTML = "1";
-        //Reset score
-        document.getElementById("user-score").innerHTML = "0";
-        document.getElementById("cpu-score").innerHTML = "0";
-        document.getElementById("draw-score").innerHTML = "0";
-        //TODO: break out of function? not sure if this is the way to do it
-        return;
+
+        document.getElementById("rock").disabled = true;
+        document.getElementById("paper").disabled = true;
+        document.getElementById("scissors").disabled = true;
     }
 
-    roundNumber++;
-    document.getElementById("round-number").innerHTML = roundNumber;
+    if (roundNumber < 5) {
+        roundNumber++;
+        document.getElementById("round-number").innerHTML = roundNumber;
+    }
+
+
+}
+
+function reset() {
+    // Reset results to blank
+    document.getElementById("results").innerHTML = "";
+    // Reset roundNumber to 1
+    document.getElementById("round-number").innerHTML = "1";
+    // Reset score
+    document.getElementById("user-score").innerHTML = "0";
+    document.getElementById("cpu-score").innerHTML = "0";
+    document.getElementById("draw-score").innerHTML = "0";
+
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
 }
 
 const rock = document.querySelector('#rock');
-rock.addEventListener('click', function(){playRound('rock', computerPlay())});
+rock.addEventListener('click', function () { playRound('rock', computerPlay()) });
 
 const paper = document.querySelector('#paper');
-paper.addEventListener('click', function(){playRound('paper', computerPlay())});
+paper.addEventListener('click', function () { playRound('paper', computerPlay()) });
 
 const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', function(){playRound('scissors', computerPlay())});
+scissors.addEventListener('click', function () { playRound('scissors', computerPlay()) });
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', function () { reset() });
 
 //game();
